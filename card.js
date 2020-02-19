d7494df653042336d0e1b937d2ab4fbda49096c4
Dave Pernitz <pernitz@uwalumni.com>
2020-02-19 15:15:31 -0700

    Add coment to card.js

--- card.js
+++ card.js
@@ -1,10 +1,11 @@
 class Card {
   constructor(matchInfo) {
+    // need to match other same pic, 1,2,3,4,5 or A,B,C,D,E
     this.matchInfo = matchInfo;
     this.matched = false;
   }
   match() {
-    
+
     // this.matched= true;
   }
 }
